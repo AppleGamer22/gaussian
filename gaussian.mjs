0216eff4ddfd1c488f7ff1da74f0d61feb3df575
Omri Bornstein <omribor@gmail.com>
2023-10-02 11:38:43 +1100

    Update gaussian.mjs

--- gaussian.mjs
+++ gaussian.mjs
@@ -5,4 +5,5 @@ const variance = 1;
 
 const distribution = gaussian(mean, variance);
 const n = parseInt(process.argv[2]);
+// seed is not set, random number sequence would be different every time
 console.log(distribution.random(n).join('\n'));
